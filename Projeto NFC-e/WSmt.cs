585db9d61a7577f229a91e10c1938f52be6c383c
toniellenk <toniel_lm@hotmail.com>
2015-09-01 13:47:19 -0400

    Estruturação dos métodos e inclusão do Consulta Nf-e

--- Projeto NFC-e/WSmt.cs
+++ Projeto NFC-e/WSmt.cs
@@ -15,10 +15,10 @@ namespace Projeto_NFC_e
     class WSmt
     {
          private XmlNode XmlEnvio;
-         private sefaznfe.NfeStatusServico2 wsSer = new sefaznfe.NfeStatusServico2();
-         public WSmt()
-        {
+         
+        Public void MontarXmlStatServ(){    
             string WSstats = null;
+            XmlEnvio = null;
             WSstats = "<consStatServ xmlns=" + '"' + "http://www.portalfiscal.inf.br/nfe" + '"' + " versao=" + '"' + "3.10" + '"' + ">";
             WSstats += "<tpAmb>1</tpAmb>";
             WSstats += "<cUF>51</cUF>";
@@ -29,10 +29,28 @@ namespace Projeto_NFC_e
             XmlArq.PreserveWhitespace = true;
             XmlArq.LoadXml(WSstats);
             XmlEnvio = XmlArq.DocumentElement;
+        }
+        
+        Public void MontarXmlConsSit(Int ChaveNfe){    
+            string WSstats = null;
+            XmlEnvio = null;
+            WSstats = "<consStatServ xmlns=" + '"' + "http://www.portalfiscal.inf.br/nfe" + '"' + " versao=" + '"' + "3.10" + '"' + ">";
+            WSstats += "<tpAmb>1</tpAmb>";
+            WSstats += "<cUF>51</cUF>";
+            WSstats += "<xServ>CONSULTAR</xServ>";
+            WSstats += "<chNFe>"+ChaveNfe+"</chNFe>";
+            WSstats += "</consStatServ>";
 
+            System.Xml.XmlDocument XmlArq = new System.Xml.XmlDocument();
+            XmlArq.PreserveWhitespace = true;
+            XmlArq.LoadXml(WSstats);
+            XmlEnvio = XmlArq.DocumentElement;
+        }
+         
+        public string WsConsSit()
+        {
+            sefaznfe.NfeStatusServico2 wsSer = new sefaznfe.NfeStatusServico2();
             sefaznfe.nfeCabecMsg wsCab = new sefaznfe.nfeCabecMsg();
-
-
             wsCab.cUF = "51";
             wsCab.versaoDados = "3.10";
             wsSer.nfeCabecMsgValue = wsCab;
@@ -48,15 +66,47 @@ namespace Projeto_NFC_e
             string NumSerie = "4FC5BEEB6E9DCB48";
             foreach (X509Certificate2 cert in lcerts)
             {
-                if (NumSerie == cert.SerialNumber)
+                if (cert.SerialNumber == NumSerie)
                 {
-
                     wsSer.ClientCertificates.Add(cert);
+                    return wsSer.nfeStatusServicoNF2(XmlEnvio).OuterXml;
                 }
                 else
                 {
-                    MessageBox.Show("O Número de Série " + cert.SerialNumber + " não foi encontrado.");
+                    MessageBox.Show("O Número de Série " + NumSerie + " não foi encontrado.");
+                }
+            }
 
+            lStore.Close();
+        }
+        
+        public string WsConsSit()
+        {
+            SefazConsSit.NfeStatusServico2 wsSer = new SefazConsSit.NfeStatusServico2();
+            SefazConsSit.nfeCabecMsg wsCab = new SefazConsSit.nfeCabecMsg();
+            wsCab.cUF = "51";
+            wsCab.versaoDados = "3.10";
+            wsSer.nfeCabecMsgValue = wsCab;
+
+            X509Certificate2Collection lcerts;
+            X509Store lStore = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+
+            // Abre o Store
+            lStore.Open(OpenFlags.OpenExistingOnly);
+
+            // Lista os certificados
+            lcerts = lStore.Certificates;
+            string NumSerie = "4FC5BEEB6E9DCB48";
+            foreach (X509Certificate2 cert in lcerts)
+            {
+                if (cert.SerialNumber == NumSerie)
+                {
+                    wsSer.ClientCertificates.Add(cert);
+                    return wsSer.nfeConsultaNF2(XmlEnvio).OuterXml;
+                }
+                else
+                {
+                    MessageBox.Show("O Número de Série " + NumSerie + " não foi encontrado.");
                 }
             }
 
@@ -67,11 +117,6 @@ namespace Projeto_NFC_e
         {
             return XmlEnvio.OuterXml;
         }
-        public string XmlRet()
-        {
-            return wsSer.nfeStatusServicoNF2(XmlEnvio).OuterXml;
-        }
-
     }
 }
 
