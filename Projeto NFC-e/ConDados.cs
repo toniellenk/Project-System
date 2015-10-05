2fe5296fd09982986d8407d4914918dd2d189b31
toniellenk <toniel_lm@hotmail.com>
2015-10-05 13:00:24 -0400

    Update ConDados.cs

--- Projeto NFC-e/ConDados.cs
+++ Projeto NFC-e/ConDados.cs
@@ -109,6 +109,42 @@ namespace Projeto_NFC_e
                 ObjConn.Close();
             }
         
+         public void atualizar(ClientesObj clientes)
+            {
+                string SqlInsert = "update t0050 set Nome = @Nome, CpfCnpj = @CpfCnpj, Pessoa = @Pessoa, @Estrangeiro, ";
+                SqlInsert += "@IdentEstrangeiro, @RS, @NomeFant, @Endereco, ";
+                SqlInsert += "@Num, @Cep, @FoneRes, @FoneCom, ";
+                SqlInsert += "@Cel, @OutrosCont, @Email, @IdentFiscal, ";
+                SqlInsert += "@InscEst, @InscMun)";
+                SqlConnection ObjConn = new SqlConnection(SrtCon);
+                SqlCommand ObjCmd = new SqlCommand(SqlInsert, ObjConn);
+
+                ObjCmd.Parameters.AddWithValue("@Nome", clientes.Nome);
+                ObjCmd.Parameters.AddWithValue("@CpfCnpj", clientes.CpfCnpj);
+                ObjCmd.Parameters.AddWithValue("@Pessoa", clientes.Pessoa);
+                ObjCmd.Parameters.AddWithValue("@Estrangeiro", clientes.Estrangeiro);
+                ObjCmd.Parameters.AddWithValue("@IdentEstrangeiro", clientes.IdentEstrangeiro);
+                ObjCmd.Parameters.AddWithValue("@RS", clientes.RS);
+                ObjCmd.Parameters.AddWithValue("@NomeFant", clientes.NomeFant);
+                ObjCmd.Parameters.AddWithValue("@Endereco", clientes.Endereco);
+                ObjCmd.Parameters.AddWithValue("@Num", clientes.Num);
+                ObjCmd.Parameters.AddWithValue("@Cep", clientes.Cep);
+                ObjCmd.Parameters.AddWithValue("@FoneRes", clientes.FoneRes);
+                ObjCmd.Parameters.AddWithValue("@FoneCom", clientes.FoneCom);
+                ObjCmd.Parameters.AddWithValue("@Cel", clientes.Cel);
+                ObjCmd.Parameters.AddWithValue("@OutrosCont", clientes.OutrosCont);
+                ObjCmd.Parameters.AddWithValue("@Email", clientes.Email);
+                ObjCmd.Parameters.AddWithValue("@IdentFiscal", clientes.IdentFiscal);
+                ObjCmd.Parameters.AddWithValue("@InscEst", clientes.InscEst);
+                ObjCmd.Parameters.AddWithValue("@InscMun", clientes.InscMun);
+                
+                ObjConn.Open();
+                
+                ObjCmd.ExecuteNonQuery();
+                
+                ObjConn.Close();
+            }
+        
         public string nome {
             get { return SrtCon; }
         }
