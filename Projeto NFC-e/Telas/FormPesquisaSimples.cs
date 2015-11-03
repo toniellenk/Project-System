a94ddb02317bb357d75461129b3153c8001a0ed8
Toniel Lenk <toniel_lm@hotmail.com>
2015-11-03 12:45:43 -0300

    Update FormPesquisaSimples.cs

--- Projeto NFC-e/Telas/FormPesquisaSimples.cs
+++ Projeto NFC-e/Telas/FormPesquisaSimples.cs
@@ -18,25 +18,22 @@ namespace Projeto_NFC_e
         public FormPesquisaSimples()
         {
             InitializeComponent();
+        }
 
+        private void FormPesquisaSimples_Load(object sender, EventArgs e)
+        {
             CarregarListView();
 
             Dictionary<string, string> comboSource = new Dictionary<string, string>();
    
-            comboSource.Add("ID", "1");
-            comboSource.Add("DESCRIÇÃO", "2");
+            comboSource.Add("1", "ID");
+            comboSource.Add("2", "DESCRIÇÃO");
 
             CombBxFilt.DataSource = new BindingSource(comboSource, null);
             CombBxFilt.DisplayMember = "Value";
             CombBxFilt.ValueMember = "Key";
 
             CombBxFilt.SelectedIndex = CombBxFilt.FindStringExact("DESCRIÇÃO");
-
-        }
-
-        private void FormPesquisaSimples_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void PanFiltros_Paint(object sender, PaintEventArgs e)
@@ -53,14 +50,12 @@ namespace Projeto_NFC_e
             TipoFilt = Controles.BotaoFiltro.FiltCliente;
 
             LsVyPrinc.DataSource = Controles.CarregarGradeRapida("","Cidade");
-          //  LsVyPrinc.Columns[0].Width = 30;
-          //  LsVyPrinc.Columns[1].DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            LsVyPrinc.Columns[0].Width = 30;
+            LsVyPrinc.Columns[1].DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
 
             LsVyPrinc.Visible = true;
             PanFiltros.Visible = true;
-
-
-
         }
+        
     }
 }
