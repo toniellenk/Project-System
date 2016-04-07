2a35a87f856b3b52b5ebf37093400451bcfce3a5
Toniel Lenk <toniel_lm@hotmail.com>
2016-04-07 14:00:26 -0400

    07042016

--- Projeto NFC-e/Telas/PesquisaSimples/PesquisaClientePdVenda.cs
+++ Projeto NFC-e/Telas/PesquisaSimples/PesquisaClientePdVenda.cs
@@ -67,5 +67,54 @@ namespace Projeto_NFC_e
                             }
 
         }
+        
+        
+        public override void CarregarFormBase(string Item)
+        {
+
+            string colunas = "t0050.Nome as NomeCliente, t0050.CpfCnpj, t0050.Endereco,t0050.SitCredito, t0030.Nome as NomeCidade, t0029.Sigla";
+            DadosClientes Cliente = new DadosClientes();
+            Cliente.Consulta(colunas, "where t0050.IdCliente = "+ Item);
+            foreach (DataRow dr in Cliente.dt.Rows)
+                            {
+
+                                string CpfCnpj = Funcoes.FormataCpfCnpj(dr["CpfCnpj"].ToString());
+
+                                string SitCredito = dr["SitCredito"].ToString();
+
+                                SitCredito = SitCredito.Trim();
+
+                                if (SitCredito == "") SitCredito = "2";
+
+                                 if (Convert.ToInt32(SitCredito) == 1)
+                                {
+                                    SecaoFormPdVenda.LabSitCred.Text = "Aprovado";
+                                    SecaoFormPdVenda.ImgOk.Visible = true;
+                                    SecaoFormPdVenda.ImgAlert.Visible = false;
+                                    SecaoFormPdVenda.ImgBloq.Visible = false;
+                                }
+                                if (Convert.ToInt32(SitCredito) == 2)
+                                {
+                                    SecaoFormPdVenda.LabSitCred.Text = "Pendente";
+                                    SecaoFormPdVenda.ImgAlert.Visible = true;
+                                    SecaoFormPdVenda.ImgOk.Visible = false;
+                                    SecaoFormPdVenda.ImgBloq.Visible = false;
+                                }
+                                if (Convert.ToInt32(SitCredito) == 3)
+                                {
+                                    SecaoFormPdVenda.LabSitCred.Text = "Bloqueado";
+                                    SecaoFormPdVenda.ImgBloq.Visible = true;
+                                    SecaoFormPdVenda.ImgAlert.Visible = false;
+                                    SecaoFormPdVenda.ImgAlert.Visible = false;
+                                }
+
+                                SecaoFormPdVenda.LabNomeCliente.Text = dr["NomeCliente"].ToString();
+                                SecaoFormPdVenda.LabCpfCnpjCliente.Text = CpfCnpj;
+                                SecaoFormPdVenda.LabEnderCliente.Text = dr["Endereco"].ToString();
+                                SecaoFormPdVenda.LabCidade.Text = dr["NomeCidade"].ToString();
+                                SecaoFormPdVenda.LabUf.Text = dr["Sigla"].ToString();
+                            }
+
+        }
     }
 }
